6e78786b762492b51de3749e56249999e7934e0e
CrinaPopovici <74511992+CrinaPopovici@users.noreply.github.com>
2023-11-27 17:05:16 +0200

    Exercising CRUD operations

---
+++ Homework2/Homework2/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Workshop15noiembrie.Controllers;
+[Route("base")]
+[ApiController]
+public class FirstController : ControllerBase
+{
+    public static List<string> _list = new List<string>()
+    {
+        "first", "second"
+    };
+    [HttpGet] 
+    public ActionResult Get() //ActionResult inseamna ca trebuie sa returnam un status code
+    {
+        return Ok(_list);
+    }
+    [HttpPost] 
+    public ActionResult Post([FromBody] string str) //ActionResult inseamna ca trebuie sa returnam un status code
+    {
+        _list.Add(str);
+        return Ok(_list);
+    }
+    
+}
